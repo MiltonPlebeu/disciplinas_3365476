00d3a29670ff299257b7fb9a29374a414f4831a6
milton brasileiro <mplebeu@gmail.com>
2018-08-24 15:08:41 -0300

    Fix #4 Criar formulário para cadastro de disciplinas

--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,3 +1,4 @@
+<a href="/disciplinas/create"><h1>Cadastrar disciplina</h1></a>
 <ul>
 @foreach ($disciplinas as $disciplina)
 	<li><a href="/disciplinas/{{$disciplina->id}}"><h1>{{ $disciplina->titulo }}</h1></a></li>
