7b54fabe8a6a91ec4a7514102abbc707bd4ab326
milton brasileiro <mplebeu@gmail.com>
2018-08-28 17:30:35 -0300

    Resolução da #Issue5

--- app/Http/Controllers/DisciplinaController.php
+++ app/Http/Controllers/DisciplinaController.php
@@ -66,7 +66,7 @@ class DisciplinaController extends Controller
      */
     public function edit(Disciplina $disciplina)
     {
-        //
+        return view('disciplinas.edit',compact('disciplina'));
     }
 
     /**
@@ -78,7 +78,11 @@ class DisciplinaController extends Controller
      */
     public function update(Request $request, Disciplina $disciplina)
     {
-        //
+        $disciplina->titulo = $request->titulo;
+        $disciplina->ementa = $request->ementa;
+        $disciplina->save();
+
+        return redirect("/disciplinas/$disciplina->id");
     }
 
     /**
--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,6 +1,12 @@
 <a href="/disciplinas/create"><h1>Cadastrar disciplina</h1></a>
-<ul>
-@foreach ($disciplinas as $disciplina)
-	<li><a href="/disciplinas/{{$disciplina->id}}"><h1>{{ $disciplina->titulo }}</h1></a></li>
-@endforeach
-</ul>
+    <ul>
+    @foreach ($disciplinas as $disciplina)
+        <li>
+            <a href="/disciplinas/{{ $disciplina->id }}">
+                {{ $disciplina->titulo }} 
+            </a>
+            <br>
+            <a href="/disciplinas/{{ $disciplina->id }}/edit"> Editar </a>
+        </li>
+    @endforeach
+    </ul>
